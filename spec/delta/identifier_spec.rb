8c7e96512670102229f5c57466c03620df8618c3
Chris Patuzzo <chris@patuzzo.co.uk>
2015-05-23 23:30:16 +0100

    Unit test Delta::Identifier

---
+++ spec/delta/identifier_spec.rb
@@ -0,0 +1,36 @@
+require "spec_helper"
+
+RSpec.describe Delta::Identifier do
+
+  it "returns an array of attributes specified by the initializer" do
+    subject = described_class.new([:size, :to_s])
+    expect(subject.identity([1, 2])).to eq([2, "[1, 2]"])
+
+    subject = described_class.new([:reverse, :upcase])
+    expect(subject.identity("foo")).to eq(%w(oof FOO))
+  end
+
+  it "cannot call private methods on the object" do
+    class SomethingWithAPrivateMethod
+      private
+
+      def a_private_method
+      end
+    end
+
+    subject = described_class.new([:a_private_method])
+
+    expect { subject.identity(SomethingWithAPrivateMethod.new) }
+      .to raise_error(NoMethodError)
+  end
+
+  describe "null object" do
+    subject { described_class::Null.new }
+
+    it "returns the given object" do
+      object = Object.new
+      expect(subject.identity(object)).to eq(object)
+    end
+  end
+
+end
