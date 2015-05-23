f5725a86db391628a4f8b88fbcaa612665ff5218
Chris Patuzzo <chris@patuzzo.co.uk>
2015-05-23 16:39:36 +0100

    Implementation for the simple example

--- lib/delta.rb
+++ lib/delta.rb
@@ -1,2 +1,38 @@
 class Delta
+
+  def initialize(from:, to:)
+    @from = from
+    @to = to
+  end
+
+  def additions
+    Enumerator.new do |y|
+      subtract(to, from).each do |element|
+        y.yield element
+      end
+    end
+  end
+
+  def modifications
+    Enumerator.new do |y|
+
+    end
+  end
+
+  def deletions
+    Enumerator.new do |y|
+      subtract(from, to).each do |element|
+        y.yield element
+      end
+    end
+  end
+
+  private
+
+  def subtract(a, b)
+    a.reject { |e| b.include?(e) }
+  end
+
+  attr_reader :from, :to
+
 end
