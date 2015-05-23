9249a7fe7aac862b367dc12beb837bd21118fba9
Chris Patuzzo <chris@patuzzo.co.uk>
2015-05-23 18:48:17 +0100

    Implement the 'pluck' example

--- lib/delta.rb
+++ lib/delta.rb
@@ -1,13 +1,14 @@
 class Delta
-  def initialize(from:, to:)
+  def initialize(from:, to:, pluck: nil)
     @from = from
     @to = to
+    @pluck = pluck
   end
 
   def additions
     Enumerator.new do |y|
       subtract(to, from).each do |element|
-        y.yield element
+        y.yield pluck_fields(element)
       end
     end
   end
@@ -21,20 +22,32 @@ class Delta
   def deletions
     Enumerator.new do |y|
       subtract(from, to).each do |element|
-        y.yield element
+        y.yield pluck_fields(element)
       end
     end
   end
 
   private
 
-  attr_reader :from, :to
+  attr_reader :from, :to, :pluck
 
+  # TODO: Inject a collection adapter?
   def subtract(a, b)
     a.reject do |a_element|
       b.any? do |b_element|
         a_element == b_element
       end
     end
+  end
+
+  def pluck_fields(element)
+    return element unless pluck
+
+    fields = pluck.map { |p| element.public_send(p) }
+    struct.new(*fields)
+  end
+
+  def struct
+    @struct ||= Struct.new(*pluck)
   end
 end
--- spec/delta_spec.rb
+++ spec/delta_spec.rb
@@ -22,4 +22,32 @@ RSpec.describe Delta do
     expect(delta.deletions.to_a).to eq [pikachu, magikarp]
   end
 
+  it "works for the pluck example in the readme" do
+    delta = described_class.new(from: from, to: to, pluck: [:name, :species])
+
+    expect(delta.additions.count).to eq 2
+    expect(delta.modifications.count).to eq 0
+    expect(delta.deletions.count).to eq 2
+
+    first, second = *delta.additions
+
+    expect(first.name).to eq("Zappy")
+    expect(first.species).to eq("Raichu")
+    expect(first).to_not respond_to(:type)
+
+    expect(second.name).to eq("Flappy")
+    expect(second.species).to eq("Butterfree")
+    expect(second).to_not respond_to(:type)
+
+    first, second = *delta.deletions
+
+    expect(first.name).to eq("Zappy")
+    expect(first.species).to eq("Pikachu")
+    expect(first).to_not respond_to(:type)
+
+    expect(second.name).to eq("Splashy")
+    expect(second.species).to eq("Magikarp")
+    expect(second).to_not respond_to(:type)
+  end
+
 end
