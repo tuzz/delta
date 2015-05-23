d3e5856c8623190ca4e0876b9d5ec7621cc3c53d
Chris Patuzzo <chris@patuzzo.co.uk>
2015-05-23 16:27:47 +0100

    Test the simple example in the readme

--- spec/delta_spec.rb
+++ spec/delta_spec.rb
@@ -1,7 +1,25 @@
 require "spec_helper"
 
 RSpec.describe Delta do
+  let(:pikachu)    { Pokemon.new("Pikachu",    "Zappy",    29, "Electric") }
+  let(:pidgey)     { Pokemon.new("Pidgey",     "Mr. Peck", 07, "Flying")   }
+  let(:magikarp)   { Pokemon.new("Magikarp",   "Splashy",  05, "Water")    }
+  let(:raichu)     { Pokemon.new("Raichu",     "Zappy",    30, "Electric") }
+  let(:butterfree) { Pokemon.new("Butterfree", "Flappy",   20, "Flying")   }
 
-  it "has some specs"
+  it "works for the simple example in the readme" do
+    delta = described_class.new(
+      from: [pikachu, pidgey, magikarp].to_enum,
+      to: [raichu, pidgey, butterfree].to_enum
+    )
+
+    expect(delta.additions).to be_an(Enumerator)
+    expect(delta.modifications).to be_an(Enumerator)
+    expect(delta.deletions).to be_an(Enumerator)
+
+    expect(delta.additions.to_a).to eq [raichu, butterfree]
+    expect(delta.modifications.to_a).to eq []
+    expect(delta.deletions.to_a).to eq [pikachu, magikarp]
+  end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,15 @@ RSpec.configure do |config|
   config.disable_monkey_patching!
   config.formatter = :doc
 end
+
+class Pokemon
+  attr_reader :species, :name, :level, :type
+
+  def initialize(species, name, level, type)
+    @species = species
+    @name = name
+    @level = level
+    @type = type
+  end
+end
+
